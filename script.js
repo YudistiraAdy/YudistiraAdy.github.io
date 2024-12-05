fe231fb794ef2a71cf6eb55fe174fd27551c7912
Yudistira Adyatama <142817359+YudistiraAdy@users.noreply.github.com>
2024-12-05 20:54:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,50 @@
 // Get the modal
 var modal = document.getElementById("modal");
-console.log("Modal:", modal);
+
+// Get the header
+var header = document.querySelector("header");
 
 // Get the image and video elements inside the modal
 var modalImg = document.getElementById("modal-img");
 var modalVideo = document.getElementById("modal-video");
-console.log("Modal Image:", modalImg);
-console.log("Modal Video:", modalVideo);
 
 // Get the title and description elements inside the modal
 var modalTitle = document.querySelector(".modal-text h2");
 var modalDescription = document.querySelector(".modal-text p");
-console.log("Modal Title:", modalTitle);
-console.log("Modal Description:", modalDescription);
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-console.log("Close Button:", span);
 
 // Add event listener to all images in projects and gallery sections
 var images = document.querySelectorAll("#projects img, #gallery img");
-console.log("Images:", images);
 images.forEach(img => {
     img.addEventListener("click", function() {
-        console.log("Image clicked:", this.src);
         modal.style.display = "block";
         modalImg.style.display = "block";
         modalVideo.style.display = "none";
         modalImg.src = this.src;
         modalTitle.textContent = this.getAttribute("data-title");
         modalDescription.textContent = this.getAttribute("data-description");
+        header.style.display = "none"; // Hide header
     });
 });
 
 // Add event listener to all videos in videos section
 var videos = document.querySelectorAll("#videos video");
-console.log("Videos:", videos);
 videos.forEach(video => {
     video.addEventListener("click", function() {
-        console.log("Video clicked:", this.querySelector("source").src);
         modal.style.display = "block";
         modalImg.style.display = "none";
         modalVideo.style.display = "block";
         modalVideo.src = this.querySelector("source").src;
         modalTitle.textContent = this.getAttribute("data-title");
         modalDescription.textContent = this.getAttribute("data-description");
+        header.style.display = "none"; // Hide header
     });
 });
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-    console.log("Close button clicked");
     modal.style.display = "none";
-}
+    header.style.display = "block"; // Show header
+};
